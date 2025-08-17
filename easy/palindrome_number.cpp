a6f032613b98e47700ed1a815e582fa57f9c8cc4
Farrel <farreluken@gmail.com>
2025-08-17 21:11:55 +0800

    oke

--- easy/palindrome_number.cpp
+++ easy/palindrome_number.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     bool isPalindrome(int x) {
-        return true;
+        if (x < 0){
+            return false;
+        }
+        
+        string textNumber = to_string(x);
+        string backwards = "";
+        int text_length = textNumber.length();
+
+        for (int i = text_length - 1; i >= 0; i--){
+            backwards += textNumber[i];
+        }
+
+        return textNumber == backwards;
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int num;
+    cin >> num;
+
+    if(Solution().isPalindrome(num)){
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+
+    return 0;
+}
